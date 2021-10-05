99902c558dd07d68f558f10d506c623e26ac161b
ethanknights <ethanknights@hotmail.co.uk>
2021-10-05 15:08:35 +0100

    fix setupDir routine for new dir structure with ROI subDirs

--- MEG/symLinkNewStartingDir.m
+++ MEG/symLinkNewStartingDir.m
@@ -1,14 +1,19 @@
 %% Specify Analysis Branch (e.g. which ROIs for directory name)
-descript_roisName = 'craddock'; %'craddock' | 'OSL_noOverlap';
-switch descript_roisName
-  case 'OSL_noOverlap'
-  case 'craddock'
-end
+% descript_roisName = 'craddock'; %'craddock' | 'OSL_noOverlap';
+% switch descript_roisName
+%   case 'OSL_noOverlap'
+%   case 'craddock'
+% end
 
 %% ========================================================================
 %% Symlink above directory, to a new pp/<roiName> directory
 %% ========================================================================
-mkdir(sprintf('data/pp/%s',descript_roisName))
-cd(sprintf('data/pp/%s',descript_roisName))
-!lndir.sh ../../AA_movecomp/ ./
-cd ../../../
+% mkdir(sprintf('data/pp/%s',descript_roisName))
+% cd(sprintf('data/pp/%s',descript_roisName))
+% !lndir.sh ../../AA_movecomp/ ./
+% cd ../../../
+
+mkdir('data/pp/')
+cd('data/pp/')
+!lndir.sh ../AA_movecomp/ ./
+cd ../../
