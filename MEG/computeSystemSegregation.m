b6176f2a5696a98e47b2fff5b13b55abc25438de
ethanknights <ethanknights@hotmail.co.uk>
2021-11-15 14:36:08 +0000

    done - add root-master plot_groupCorrM.m

--- MEG/computeSystemSegregation.m
+++ MEG/computeSystemSegregation.m
@@ -4,7 +4,7 @@
 %% plot_groupCorrMat('craddock','byNetwork')
 
 %% ==========================================================================
-function [W,B,S] = computeSystemSegregation(descript_roisName,descript_roiOrder)
+function [W,B,S] = computeSystemSegregation(descript_roisName,descript_roiOrder,doOrthog)
 
 %--- data ---%
 rootDir = pwd;
@@ -19,12 +19,25 @@ outDir = fullfile(rootOutDir,['ROIs-',descript_roisName]); mkdir(outDir)
 
 %% Get ROI Labels & Reorder
 switch descript_roisName
-  %--------------------------------------------------------------------------
-  %--------
-  case 'craddock'
+%--------------------------------------------------------------------------
+  %========
+  case 'Schaefer_100parcels_7networks'
+  %========
+    [t] = readtable('ROIs/Schaefer/Schaefer2018_100Parcels_7Networks_order_FSLMNI152_2mm_labels.txt');
     %--------
-    [t] = readtable('ROIs/craddock-ROI-835_resampled-6mm.csv');
     switch descript_roiOrder
+    %--------
+      case 'dropSchaefer'
+        reorder_dropSchaefer
+    end
+    
+  %=======
+  case 'craddock'
+  %=======
+    [t] = readtable('ROIs/craddock-ROI-835_resampled-6mm.csv');
+    %--------
+    switch descript_roiOrder
+    %--------
       case 'byNetwork'
         load('reorderIdx_atlas-craddock_order-byNetwork_nRois-835.mat','idx')
         t = t(idx,:);      roiLabels = t.networkName;   roiNetwork = t.networkIdx; %reorder
@@ -37,8 +50,8 @@ end
 %% store mean of all subjects in corrMat.delta, corrMat.theta etc
 for b=1:length(list_bandNames); bandName = list_bandNames{b};
   
-  fileStr = sprintf('data/pp/sub-CC*/ROIs-%s/hilbertEnvCorr_band-%s_roiOrder-%s.mat',...
-    descript_roisName,bandName,descript_roiOrder);
+  fileStr = sprintf('data/pp/sub-CC*/ROIs-%s/hilbertEnvCorr_band-%s_roiOrder-%s_doOrthog-%s.mat',...
+    descript_roisName,bandName,descript_roiOrder,num2str(doOrthog));
   dirContents = dir(fileStr);
   nSubs = length(dirContents);
   fprintf('%d subs found for query:\n%s\n',nSubs,fileStr)
@@ -49,35 +62,46 @@ for b=1:length(list_bandNames); bandName = list_bandNames{b};
     corrM(:,:,s) = tmpD.corrMat;
   end
   
-  %% drop networks
-  newLabels = roiLabels;
-  newNetworkIdx = roiNetwork;
-  %listNetworkStrToDrop = {'noNetwork'};
-  listNetworkStrToDrop = {'noNetwork','SMN','auditory','basal ganglia', ...
-    'brainstem','cerebellum','thalamus','visual'};
-  for n = 1:length(listNetworkStrToDrop)
-    idx = contains(newLabels,listNetworkStrToDrop{n});
-    corrM(:,idx,:) = []; corrM(idx,:,:) = [];
-    newLabels(idx) = [];
-    newNetworkIdx(idx) = [];
+  %% Get networkIdx for calculating SyS
+  switch descript_roisName
+    
+    %=============
+    case 'craddock'
+    %=============
+      %% drop networks (if craddock atlas)
+      newLabels = roiLabels;
+      newNetworkIdx = roiNetwork;
+      %listNetworkStrToDrop = {'noNetwork'};
+      listNetworkStrToDrop = {'noNetwork','SMN','auditory','basal ganglia', ...
+        'brainstem','cerebellum','thalamus','visual'};
+      for n = 1:length(listNetworkStrToDrop)
+        idx = contains(newLabels,listNetworkStrToDrop{n});
+        corrM(:,idx,:) = []; corrM(idx,:,:) = [];
+        newLabels(idx) = [];
+        newNetworkIdx(idx) = [];
+      end
+      %% print number of ROis per network left
+      u = unique(newLabels);
+      for uu = 1:length(u)
+        nRoisPerNetwork(uu) = sum(strcmp(u(uu),newLabels));
+        fprintf('nROis for Network: %s %d\n',u{uu},nRoisPerNetwork(uu));
+      end
+      %% networkIdx
+      networkIdx = newNetworkIdx;
+      
+    %=============
+    case 'Schaefer_100parcels_7networks'
+    %=============
+      networkIdx = t.Var6;
   end
-  
-  
-  %% print number of ROis per network left
-  u = unique(newLabels);
-  for uu = 1:length(u)
-    nRoisPerNetwork(uu) = sum(strcmp(u(uu),newLabels));
-    fprintf('nROis for Network: %s %d\n',u{uu},nRoisPerNetwork(uu));
-  end
-  
 
+  
   %% WIGG'S VERSION: calculate system segregation
   for s = 1:length(CCIDList)
     
     %Get main data (i.e. subs connectivity matrix & roi labels)
     M = squeeze(corrM(:,:,s));
-    Ci = newNetworkIdx;
-    
+    Ci = networkIdx;
     nCi = unique(Ci);
     
     Wv = [];
@@ -105,7 +129,8 @@ for b=1:length(list_bandNames); bandName = list_bandNames{b};
   plotRegression(S',age)
   title('System segregation - rest FC');
   xlabel('age'); ylabel('mean within - between system')
-  outName = fullfile(outDir,sprintf('SystemSegregation_%s_associationOnly',bandName));
+  outName = fullfile(outDir,sprintf('SystemSegregation_%s_associationOnly_doOrthog-%s',...
+    bandName,num2str(doOrthog)));
   cmdStr = sprintf('export_fig %s.png',outName)
   eval(cmdStr);
   h = gcf; savefig(gcf,[outName,'.fig']);
@@ -113,7 +138,8 @@ for b=1:length(list_bandNames); bandName = list_bandNames{b};
   plotRegression(B',age)
   title('Between System mean - rest FC');
   xlabel('age'); ylabel('mean between system')
-  outName = fullfile(outDir,sprintf('BetweenSystemMean_%s_associationOnly',bandName));
+  outName = fullfile(outDir,sprintf('BetweenSystemMean_%s_associationOnly_doOrthog-%s',...
+    bandName,num2str(doOrthog)));
   cmdStr = sprintf('export_fig %s.png',outName)
   eval(cmdStr);
   h = gcf; savefig(gcf,[outName,'.fig']);
@@ -121,14 +147,16 @@ for b=1:length(list_bandNames); bandName = list_bandNames{b};
   plotRegression(W',age)
   title('Within System mean - rest FC');
   xlabel('age'); ylabel('mean within system')
-  outName = fullfile(outDir,sprintf('WithinSystemMean_%s_associationOnly',bandName));
+  outName = fullfile(outDir,sprintf('WithinSystemMean_%s_associationOnly_doOrthog-%s',...
+    bandName,num2str(doOrthog)));
   cmdStr = sprintf('export_fig %s.png',outName)
   eval(cmdStr);
   h = gcf; savefig(gcf,[outName,'.fig']);
   
   toWrite = table(CCIDList,S',age);
   toWrite.Properties.VariableNames = {'CCID','SyS','Age'};
-  writetable(toWrite,fullfile(outDir,sprintf('SySTable_%s.csv',bandName)))
+  writetable(toWrite,fullfile(outDir,sprintf('SySTable_%s_doOrthog-%s.csv',...
+    bandName,num2str(doOrthog))))
 
 end
 
@@ -136,10 +164,12 @@ close all
 
 %% gather all bands in 1 table
 for b=1:length(list_bandNames); bandName = list_bandNames{b};
-  tmpT = readtable(fullfile(outDir,sprintf('SySTable_%s.csv',bandName)));
+  tmpT = readtable(fullfile(outDir,sprintf('SySTable_%s_doOrthog-%s.csv',...
+    bandName,num2str(doOrthog))));
   tmpD(:,b) = table2array(tmpT(:,2));
 end
 newTable = tmpT;
 newTable.SyS = [];
 [newTable] = appendToTable(tmpD,list_bandNames,newTable);
-writetable(newTable,fullfile(outDir,'SySTable_allBands.csv'))
+writetable(newTable,fullfile(outDir,sprintf('SySTable_allBands_doOrthog-%s.csv',...
+  num2str(doOrthog))));
--- plot_GroupCorrM/wrapper.m
+++ plot_GroupCorrM/wrapper.m
@@ -68,7 +68,7 @@ doPlot(tmpD,roiLabels,titleStr,oN)
 %% 1. with Orthog                       | (Original)
 %% ========================================================================
 dirfN = fullfile(projectDir,...
-  'MEG/data/group_corrMat/ROIs-Schaefer_100parcels_7networks_v1_orthog');
+  'MEG/data/group_corrMat/ROIs-Schaefer_100parcels_7networks_version-original');
 load(fullfile(dirfN,'group_corrMat_roiOrder-dropSchaefer_doOrthog-1.mat'),...
   'corrMat','roiLabels'); 
 %% normalise output
@@ -78,7 +78,6 @@ for b=1:length(list_bandNames); bandName = list_bandNames{b};
   %% data
   eval(sprintf('tmpD = corrMat.%s;',bandName)); %tmpD
   %% ROILabels
-  %roiLabels = networkLabel_str; %TMP!!
   [roiLabels] = makeROILabels(networkLabel_str);
   %% titleStr
   atlasName = 'Schaefer';
@@ -91,7 +90,107 @@ for b=1:length(list_bandNames); bandName = list_bandNames{b};
   doPlot(tmpD,roiLabels,titleStr,oN)
 end
 
+%% MEG
+%% 2. without orthog                    | (To see effect of orthog!)
+%% ========================================================================
+dirfN = fullfile(projectDir,...
+  'MEG/data/group_corrMat/ROIs-Schaefer_100parcels_7networks_version-original');
+load(fullfile(dirfN,'group_corrMat_roiOrder-dropSchaefer_doOrthog-0.mat'),...
+  'corrMat','roiLabels');
+%% normalise output
+%% ------------------------------------------------------------------------
+networkLabel_str = roiLabels; clear roiLabels
+for b=1:length(list_bandNames); bandName = list_bandNames{b};
+  %% data
+  eval(sprintf('tmpD = corrMat.%s;',bandName)); %tmpD
+  %% ROILabels
+  [roiLabels] = makeROILabels(networkLabel_str);
+  %% titleStr
+  atlasName = 'Schaefer';
+  nSubs = 619; %HARDCODED!!
+  titleStr = sprintf('Correlation Matrix-MEG Measure-%s Atlas-%s N-%d (Without Orthogonalisation)',bandName,atlasName,nSubs);
+  %% outName
+  oN = sprintf('%s/corrM-MEG_Measure-%s_Atlas-%s_N-%d_WithoutOrthogonalisation',outDir,bandName,atlasName,nSubs); %.jpg
+  %% doPlot
+  %% ------------------------------------------------------------------------
+  doPlot(tmpD,roiLabels,titleStr,oN)
+end
 
 %% MEG
+%% 3. with orthog, partial correlation  | (is there more lateralised structure?)
+%% ========================================================================
+dirfN = fullfile(projectDir,...
+  'MEG/data/group_corrMat/ROIs-Schaefer_100parcels_7networks_version-partialCorrelation');
+load(fullfile(dirfN,'group_corrMat_roiOrder-dropSchaefer_doOrthog-1.mat'),...
+  'corrMat','roiLabels');
+%% normalise output
+%% ------------------------------------------------------------------------
+networkLabel_str = roiLabels; clear roiLabels
+for b=1:length(list_bandNames); bandName = list_bandNames{b};
+  %% data
+  eval(sprintf('tmpD = corrMat.%s;',bandName)); %tmpD
+  %% ROILabels
+  [roiLabels] = makeROILabels(networkLabel_str);
+  %% titleStr
+  atlasName = 'Schaefer';
+  nSubs = 619; %HARDCODED!!
+  titleStr = sprintf('Correlation Matrix-MEG Measure-%s Atlas-%s N-%d (orthog&PartialCorrelation)',bandName,atlasName,nSubs);
+  %% outName
+  oN = sprintf('%s/corrM-MEG_Measure-%s_Atlas-%s_N-%d_orthogANDPartialCorrelation',outDir,bandName,atlasName,nSubs); %.jpg
+  %% doPlot
+  %% ------------------------------------------------------------------------
+  doPlot(tmpD,roiLabels,titleStr,oN)
+end
+
+%% MEG
+%% 4. with orthog, despike envelope (i.e. despike after downsampling), no partial correlation
+%% ========================================================================
+dirfN = fullfile(projectDir,...
+  'MEG/data/group_corrMat/ROIs-Schaefer_100parcels_7networks_version-despikeEnvelope');
+load(fullfile(dirfN,'group_corrMat_roiOrder-dropSchaefer_doOrthog-1.mat'),...
+  'corrMat','roiLabels');
+%% normalise output
+%% ------------------------------------------------------------------------
+networkLabel_str = roiLabels; clear roiLabels
+for b=1:length(list_bandNames); bandName = list_bandNames{b};
+  %% data
+  eval(sprintf('tmpD = corrMat.%s;',bandName)); %tmpD
+  %% ROILabels
+  [roiLabels] = makeROILabels(networkLabel_str);
+  %% titleStr
+  atlasName = 'Schaefer';
+  nSubs = 619; %HARDCODED!!
+  titleStr = sprintf('Correlation Matrix-MEG Measure-%s Atlas-%s N-%d (despikeEnvelope)',bandName,atlasName,nSubs);
+  %% outName
+  oN = sprintf('%s/corrM-MEG_Measure-%s_Atlas-%s_N-%d_orthog-despikeEnvelope',outDir,bandName,atlasName,nSubs); %.jpg
+  %% doPlot
+  %% ------------------------------------------------------------------------
+  doPlot(tmpD,roiLabels,titleStr,oN)
+end
 
 
+%% MEG
+%% 5. with orthog, despike envelope (i.e. despike after downsampling), with partial correlation
+%% ========================================================================
+dirfN = fullfile(projectDir,...
+  'MEG/data/group_corrMat/ROIs-Schaefer_100parcels_7networks_version-despikeEnvelope-partialCorrelation');
+load(fullfile(dirfN,'group_corrMat_roiOrder-dropSchaefer_doOrthog-1.mat'),...
+  'corrMat','roiLabels');
+%% normalise output
+%% ------------------------------------------------------------------------
+networkLabel_str = roiLabels; clear roiLabels
+for b=1:length(list_bandNames); bandName = list_bandNames{b};
+  %% data
+  eval(sprintf('tmpD = corrMat.%s;',bandName)); %tmpD
+  %% ROILabels
+  [roiLabels] = makeROILabels(networkLabel_str);
+  %% titleStr
+  atlasName = 'Schaefer';
+  nSubs = 619; %HARDCODED!!
+  titleStr = sprintf('Correlation Matrix-MEG Measure-%s Atlas-%s N-%d (despikeEnvelope+partialCorrelation)',bandName,atlasName,nSubs);
+  %% outName
+  oN = sprintf('%s/corrM-MEG_Measure-%s_Atlas-%s_N-%d_orthog-despikeEnvelope-partialCorrelation',outDir,bandName,atlasName,nSubs); %.jpg
+  %% doPlot
+  %% ------------------------------------------------------------------------
+  doPlot(tmpD,roiLabels,titleStr,oN)
+end
